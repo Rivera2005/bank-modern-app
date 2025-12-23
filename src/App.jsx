8323f81ee9f4a65152b32a3065735f49cabac764
Luis Adolfo Rivera Meléndez <rivera@MacBook-Air-de-Luis.local>
2025-12-23 17:48:16 -0600

    Implement the hero section

--- src/App.jsx
+++ src/App.jsx
@@ -14,20 +14,20 @@ import {
 } from "./components";
 
 const App = () => (
-  <div className="bg-black w-full overflow-hidden">
+  <div className="bg-primary w-full overflow-hidden">
     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
       <div className={`${styles.boxWidth}`}>
         <Navbar />
       </div>
     </div>
 
-    <div className={`bg-black ${styles.flexStart}`}>
+    <div className={`bg-primary ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
         <Hero />
       </div>
     </div>
 
-    <div className={`bg-black ${styles.paddingX} ${styles.flexStart}`}>
+    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
       <div className={`${styles.boxWidth}`}>
         <Stats /> <Buisness /> <Billing /> <CardDeal /> <Testimonials />{" "}
         <Clients /> <CTA /> <Footer />
