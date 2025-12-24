96112ecc0f02c9d6503eb9555559eac34047e445
Luis Adolfo Rivera Meléndez <rivera@MacBook-Air-de-Luis.local>
2025-12-23 21:04:29 -0600

    Include Card Deal section and add rounded to bottom

--- src/components/CardDeal.jsx
+++ src/components/CardDeal.jsx
@@ -1,9 +1,25 @@
-const CardDeal = () => {
-  return (
-    <div>
-      <h1></h1>
+import { card } from "../assets";
+import styles, { layout } from "../style";
+import Button from "./Button";
+
+const CardDeal = () => (
+  <section className={layout.section}>
+    <div className={layout.sectionInfo}>
+      <h2 className={styles.heading2}>
+        Find a better card deal <br className="sm:block hidden" /> in a few easy
+        steps.
+      </h2>
+      <p className={`${styles.paragraph} max-w-117.5 mt-5`}>
+        Arcu tortor, purus un mattis at sed integer facubis. Aliquet quis
+        aliquet eget mauris tortor. Aliquet ultrices ac, ametau.
+      </p>
+      <Button styles="mt-10" />
     </div>
-  );
-};
+
+    <div className={layout.sectionImg}>
+      <img src={card} alt="card" className="w-full h-full"/>
+    </div>
+  </section>
+);
 
 export default CardDeal;
